52ca49b88d3e6ae9f4d99e2c2c8e2bbe4641f909
ss4774 <ss4774@student.uni-lj.si>
2022-12-06 15:42:23 +0100

    Added triangle definition

---
+++ src/objects/Triangle.js
@@ -0,0 +1,47 @@
+import { GPUPrimitiveTopology } from "../core/ENUM/GPUPrimitiveTopology.js";
+import { Mesh } from "./Mesh.js";
+import { Vector3 } from "../math/Vector3.js";
+import { TriangleGeometry } from "./TriangleGeometry.js";
+import { TriangleBasicMaterial } from "../materials/TriangleBasicMaterial.js";
+
+
+export class Triangle extends Mesh {
+	static DEFAULT = {
+		NAME: "",
+		TYPE: "Triangle",
+
+		VISIBLE: true,
+		FRUSTUM_CULLED: true,
+
+		GEOMETRY: new TriangleGeometry(
+			{
+				indexed: false,
+				baseGeometry: {
+					positions: [new Vector3(0, 1, 0), new Vector3(-1, -1, 0), new Vector3(1, -1, 0)],
+				}
+			}
+		),
+		MATERIAL: new TriangleBasicMaterial(),
+		PICKABLE: false,
+		PRIMITIVE: GPUPrimitiveTopology.TRIANGLE_LIST,
+	};
+
+
+	constructor(args = {}) {
+		super(
+			{
+				...args, 
+				name: (args.name !== undefined) ? args.name : Triangle.DEFAULT.NAME,
+				type: (args.type !== undefined) ? args.type : Triangle.DEFAULT.TYPE,
+			
+				visible: (args.visible !== undefined) ? args.visible : Triangle.DEFAULT.VISIBLE,
+				frustumCulled: (args.frustumCulled !== undefined) ? args.frustumCulled : Triangle.DEFAULT.FRUSTUM_CULLED,
+			
+				geometry: (args.geometry !== undefined) ? args.geometry : Triangle.DEFAULT.GEOMETRY,
+				material: (args.material !== undefined) ? args.material : Triangle.DEFAULT.MATERIAL,
+				pickable: (args.pickable !== undefined) ? args.pickable : Triangle.DEFAULT.PICKABLE,
+				primitive: (args.primitive !== undefined) ? args.primitive : Triangle.DEFAULT.PRIMITIVE,
+			}
+		);
+	}
+};
---
+++ src/objects/TriangleGeometry.js
@@ -0,0 +1,160 @@
+import { RCBufferDescriptor } from "../core/RCBufferDescriptor.js";
+import { Vector3 } from "../math/Vector3.js";
+import { MeshGeometry } from "./MeshGeometry.js";
+
+
+export class TriangleGeometry extends MeshGeometry {
+	static DEFAULT = {
+		TYPE: "TriangleGeometry",
+		NAME: "",
+	};
+	
+
+	constructor(args = {}) {
+		super(
+			{
+				...args, 
+				name: (args.name !== undefined) ? args.name : TriangleGeometry.DEFAULT.NAME,
+				type: (args.type !== undefined) ? args.type : TriangleGeometry.DEFAULT.TYPE,
+
+				indices: (args.indices !== undefined) ? args.indices : TriangleGeometry.assembleIndices(args),
+				vertices: (args.vertices !== undefined) ? args.vertices : TriangleGeometry.assembleVertices(args),
+				normals: (args.normals !== undefined) ? args.normals : TriangleGeometry.assembleNormals(args),
+				tangents: (args.tangents !== undefined) ? args.tangents : null,
+				bitangents: (args.bitangents !== undefined) ? args.bitangents : null,
+				colors: (args.colors !== undefined) ? args.colors : null,
+				uvs: (args.uvs !== undefined) ? args.uvs : TriangleGeometry.assembleUVs(args),
+				MMats: (args.MMats !== undefined) ? args.MMats : null,
+				translations: (args.translations !== undefined) ? args.translations : null,
+			}
+		);
+	}
+
+
+	static assembleIndices(args = {}) {
+		const baseGeometry = args.baseGeometry;
+		const indexed = args.indexed;
+
+		if (indexed) {
+			//TODO
+		} else {
+			return null;
+		}
+	}
+	static assembleVertices(args = {}) {
+		const baseGeometry = args.baseGeometry;
+		const indexed = args.indexed;
+		const positions = baseGeometry.positions;
+
+		if (indexed) {
+
+		} else {
+			let verticesArray = new Array();
+
+			for (let p = 0; p < positions.length; p+=3) {
+				const array = new Array(3 * 3);
+				const p1 = positions[p + 0];
+				const p2 = positions[p + 1];
+				const p3 = positions[p + 2];
+
+
+				array[0] = p1.x; array[1] = p1.y; array[2] = p1.z;
+				array[3] = p2.x; array[4] = p2.y; array[5] = p2.z;
+				array[6] = p3.x; array[7] = p3.y; array[8] = p3.z;
+
+
+				verticesArray = verticesArray.concat(array);
+			}
+
+			const verticesArrayBuffer = new Float32Array(verticesArray);
+			const verticesBufferDescriptor = new RCBufferDescriptor(
+                {
+                    array: verticesArrayBuffer,
+                    itemSize: 3,
+                }
+            )
+
+
+			return verticesBufferDescriptor;
+		}
+	}
+	static assembleNormals(args = {}) {
+		const baseGeometry = args.baseGeometry;
+		const indexed = args.indexed;
+		const positions = baseGeometry.positions;
+
+		if (indexed) {
+			//TODO
+		} else {
+			let normalsArray = new Array();
+			const a = new Vector3();
+			const b = new Vector3();
+			const n = new Vector3();
+
+			for (let p = 0; p < positions.length; p+=3) {
+				const array = new Array(3 * 3);
+				const p1 = positions[p + 0];
+				const p2 = positions[p + 1];
+				const p3 = positions[p + 2];
+
+				a.subVectors(p2, p1);
+				b.subVectors(p3, p1);
+				n.set(a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x);
+
+
+				array[0] = n.x; array[1] = n.y; array[2] = n.z;
+				array[3] = n.x; array[4] = n.y; array[5] = n.z;
+				array[6] = n.x; array[7] = n.y; array[8] = n.z;
+
+
+				normalsArray = normalsArray.concat(array);
+			}
+
+			const normalsArrayBuffer = new Float32Array(normalsArray);
+			const normalsBufferDescriptor = new RCBufferDescriptor(
+                {
+                    array: normalsArrayBuffer,
+                    itemSize: 3,
+                }
+            )
+			normalsBufferDescriptor.normalize();
+
+
+			return normalsBufferDescriptor;
+		}
+	}
+	static assembleUVs(args = {}) {
+		const baseGeometry = args.baseGeometry;
+		const indexed = args.indexed;
+		const positions = baseGeometry.positions;
+
+		if (indexed) {
+			//TODO
+		} else {
+			let uvsArray = new Array();
+
+			for (let p = 0; p < positions.length; p+=3) {
+				const array = new Array(3 * 2);
+
+
+				array[0] = +0; array[1] = +1;
+				array[2] = +0; array[3] = +0;
+				array[4] = +1; array[5] = +0;
+
+
+				uvsArray = uvsArray.concat(array);
+			}
+
+			const uvsArrayBuffer = new Float32Array(uvsArray);
+			const uvsBufferDescriptor = new RCBufferDescriptor(
+                {
+                    array: uvsArrayBuffer,
+                    itemSize: 2,
+                }
+            )
+
+
+			return uvsBufferDescriptor;
+		}
+	}
+};
