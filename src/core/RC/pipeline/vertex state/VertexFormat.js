c6c059d995a9f7075e1f348e9e098c7c71c70b9b
ss4774 <ss4774@student.uni-lj.si>
2023-02-21 16:01:44 +0100

    Added vertex format definition

---
+++ src/core/RC/pipeline/vertex state/VertexFormat.js
@@ -0,0 +1,37 @@
+export class VertexFormat { //RC vertex format
+
+
+    static UINT_8x2 = "uint8x2";
+    static UINT_8x4 = "uint8x4";
+    static SINT_8x2 = "sint8x2";
+    static SINT_8x4 = "sint8x4";
+    static UNORM_8x2 = "unorm8x2";
+    static UNORM_8x4 = "unorm8x4";
+    static SNORM_8x2 = "snorm8x2";
+    static SNORM_8x4 = "snorm8x4";
+    static UINT_16x2 = "uint16x2";
+    static UINT_16x4 = "uint16x4";
+    static SINT_16x2 = "sint16x2";
+    static SINT_16x4 = "sint16x4";
+    static UNORM_16x2 = "unorm16x2";
+    static UNORM_16x4 = "unorm16x4";
+    static SNORM_16x2 = "snorm16x2";
+    static SNORM_16x4 = "snorm16x4";
+    static FLOAT_16x2 = "float16x2";
+    static FLOAT_16x4 = "float16x4";
+    static FLOAT_32 = "float32";
+    static FLOAT_32x2 = "float32x2";
+    static FLOAT_32x3 = "float32x3";
+    static FLOAT_32x4 = "float32x4";
+    static UINT_32 = "uint32";
+    static UINT_32x2 = "uint32x2";
+    static UINT_32x3 = "uint32x3";
+    static UINT_32x4 = "uint32x4";
+    static SINT_32 = "sint32";
+    static SINT_32x2 = "sint32x2";
+    static SINT_32x3 = "sint32x3";
+    static SINT_32x4 = "sint32x4";
+
+
+    constructor(args = {}) {}
+};
