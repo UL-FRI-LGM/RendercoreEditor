d152a1539bccb025936dd0d5a24cb6e2f825ec3c
ss4774 <ss4774@student.uni-lj.si>
2023-02-21 11:24:09 +0100

    Added vertex attribute definition

---
+++ src/core/RC/pipeline/vertex state/VertexAttribute.js
@@ -0,0 +1,33 @@
+export class VertexAttribute { //RC vertex attribute
+
+
+    static DEFAULT = {
+        FORMAT: undefined,
+        OFFSET: undefined,
+
+        SHADER_LOCATION: undefined,
+    };
+
+
+    #format;
+    #offset;
+    
+    #shaderLocation;
+
+
+    constructor(args = {}) {
+        this.format = args.format !== undefined ? args.format : VertexAttribute.DEFAULT.FORMAT;
+        this.offset = args.offset !== undefined ? args.offset : VertexAttribute.DEFAULT.OFFSET;
+        
+        this.shaderLocation = args.shaderLocation !== undefined ? args.shaderLocation : VertexAttribute.DEFAULT.SHADER_LOCATION;
+    }
+
+
+    get format() { return this.#format; }
+    set format(format) { this.#format = format; }
+    get offset() { return this.#offset; }
+    set offset(offset) { this.#offset = offset; }
+
+    get shaderLocation() { return this.#shaderLocation; }
+    set shaderLocation(shaderLocation) { this.#shaderLocation = shaderLocation; }
+}
