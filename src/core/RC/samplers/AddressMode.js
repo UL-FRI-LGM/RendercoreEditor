52e9b69d3b88ba5ba07bf2e7eec4324572317b42
ss4774 <ss4774@student.uni-lj.si>
2023-02-21 15:23:07 +0100

    Added address mode definition

---
+++ src/core/RC/samplers/AddressMode.js
@@ -0,0 +1,10 @@
+export class AddressMode { //RC address mode
+
+
+    static CLAMP_TO_EDGE = "clamp-to-edge";
+    static REPEAT = "repeat";
+    static MIRROR_REPEAT = "mirror-repeat";
+
+
+    constructor() {}
+};
