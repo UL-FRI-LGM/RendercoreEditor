92a3ffd319fe1a1afe59f7163de967b2249f364c
ss4774 <ss4774@student.uni-lj.si>
2023-03-22 17:08:02 +0100

    Added compute state definition

---
+++ src/core/RC/pipeline/ComputeState.js
@@ -0,0 +1,10 @@
+import { ProgrammableStage } from "./ProgrammableStage.js";
+
+
+export class ComputeState extends ProgrammableStage {
+
+
+    constructor(args = {}) {
+        super(args);
+    }
+};
