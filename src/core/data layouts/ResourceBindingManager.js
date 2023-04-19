a4954592de5258406ce3383fcae419b7d08436c6
ss4774 <ss4774@student.uni-lj.si>
2023-04-19 15:29:44 +0200

    Added resource binding manager definition

---
+++ src/core/data layouts/ResourceBindingManager.js
@@ -0,0 +1,183 @@
+import { ObjectBase } from "../ObjectBase.js";
+import { GPUExtent3D } from "../DICTS/GPUExtent3D.js";
+import { GPUImageCopyTexture } from "../DICTS/GPUImageCopyTexture.js";
+import { GPUImageDataLayout } from "../DICTS/GPUImageDataLayout.js";
+import { GPUOrigin3D } from "../DICTS/GPUOrigin3D.js";
+import { BufferDescriptor } from "../RC/buffers/BufferDescriptor.js";
+import { SamplerDescriptor } from "../RC/samplers/SamplerDescriptor.js";
+import { TextureDescriptor } from "../RC/textures/TextureDescriptor.js";
+
+
+export class ResourceBindingManager extends ObjectBase { //RC resource binding manager
+
+
+    static DEFAULT = {
+        NAME: "",
+        TYPE: "ResourceBindingManager",
+    };
+
+
+    #context;
+
+    #descriptors = new Set();
+    #resourceBindings = new Map();
+
+
+	constructor(contextManager, args = {}) {
+        super(
+			{
+				...args,
+
+				name: (args.name !== undefined) ? args.name : ResourceBindingManager.DEFAULT.NAME,
+				type: (args.type !== undefined) ? args.type : ResourceBindingManager.DEFAULT.TYPE,
+			}
+		);
+
+        this.context = (contextManager.context !== undefined) ? contextManager.context : undefined;
+
+        this.bufferManager = contextManager.bufferManager;
+        this.textureManager = contextManager.textureManager;
+        this.samplerManager = contextManager.samplerManager;
+
+        this.descriptors = (args.descriptors !== undefined) ? args.descriptors : new Set();
+        this.resourceBindings = (args.resourceBindings !== undefined) ? args.resourceBindings : new Map();
+	}
+
+
+	get context() { return this.#context; }
+    set context(context) { this.#context = context; }
+
+    get descriptors() { return this.#descriptors; }
+    set descriptors(descriptors) { this.#descriptors = descriptors; }
+    get resourceBindings() { return this.#resourceBindings; }
+    set resourceBindings(resourceBindings) { this.#resourceBindings = resourceBindings; }
+
+
+    #createResourceBinding(descriptor) {
+        // TODO make new wrapper class for this, or context.createResourceBinding(descriptor)?
+        const resourceBinding = descriptor;
+
+        const resourceDescriptor = resourceBinding.resourceDescriptor;
+        const bindGroupLayoutEntry = resourceBinding.bindGroupLayoutEntry;
+        const bindGroupEntry = resourceBinding.bindGroupEntry;
+
+        switch (resourceDescriptor.type) {
+            case BufferDescriptor.DEFAULT.TYPE:
+                bindGroupEntry.resource.buffer = this.bufferManager.getBuffer(resourceDescriptor);
+                break;
+            case TextureDescriptor.DEFAULT.TYPE:
+                bindGroupEntry.resource = this.textureManager.getTexture(resourceDescriptor).createView();
+                break;
+            case SamplerDescriptor.DEFAULT.TYPE:
+                bindGroupEntry.resource = this.samplerManager.getSampler(resourceDescriptor);
+                break;
+            default:
+                throw new Error(`Unknown resource type: [${resourceDescriptor.type}]!`);
+        }
+
+        this.resourceBindings.set(descriptor, resourceBinding);
+
+        descriptor.dirty = false;
+
+
+        return resourceBinding;
+    }
+    createResourceBinding(descriptor) {
+        if (this.resourceBindings.has(descriptor)) this.#deleteResourceBinding(descriptor);
+        const resourceBinding = this.#createResourceBinding(descriptor);
+
+
+        return resourceBinding;
+    }
+    #updateResourceBinding(descriptor) {
+        return this.createResourceBinding(descriptor);
+    }
+    getResourceBinding(descriptor) {
+        return (this.resourceBindings.has(descriptor)) ? ((descriptor.dirty) ? this.#updateResourceBinding(descriptor) : this.resourceBindings.get(descriptor)) : this.createResourceBinding(descriptor);
+    }
+    #deleteResourceBinding(descriptor) {
+        // this.resourceBindings.get(descriptor).delete();
+        const delted = this.resourceBindings.delete(descriptor);
+
+
+        return delted;
+    }
+    deleteResourceBinding(descriptor) {
+        return (this.resourceBindings.has(descriptor)) ? this.#deleteResourceBinding(descriptor) : false;
+    }
+
+    #setBufferBinding(bufferDescriptor, setInstruction) {
+        const buffer_dst = this.bufferManager.getBuffer(bufferDescriptor);
+        const offset_dst = setInstruction.destination.layout.offset;
+        const arrayBuffer_src = setInstruction.source.arrayBuffer;
+        const offset_src = setInstruction.source.layout.offset;
+        const size = setInstruction.size;
+
+        const byteSize_dst = 4;
+        const byteSize_src = arrayBuffer_src.byteLength / arrayBuffer_src.length;
+
+        this.context.queue.writeBuffer(
+            buffer_dst,
+            offset_dst * byteSize_dst,
+            arrayBuffer_src.buffer,
+            offset_src * byteSize_src,
+            size * byteSize_src
+        );
+    }
+    #setTextureBinding(textureDescriptor, setInstruction) {    
+        const texture = this.textureManager.getTexture(textureDescriptor);
+
+        this.context.queue.writeTexture(
+            new GPUImageCopyTexture(
+                {
+                    texture: texture,
+        
+                    mipLevel: setInstruction.destination.layout.mipLevel,
+                    origin: new GPUOrigin3D(
+                        {
+                            x: setInstruction.destination.layout.x,
+                            y: setInstruction.destination.layout.y,
+                            z: setInstruction.destination.layout.z,
+                        }
+                    ),
+                    aspect: setInstruction.destination.layout.aspect,
+                }
+            ),
+            setInstruction.source.arrayBuffer.buffer,
+            new GPUImageDataLayout(
+                {
+                    offset: setInstruction.source.layout.offset,
+                    bytesPerRow: setInstruction.size.width * 4,
+                    rowsPerImage: setInstruction.size.height,
+                }
+            ),
+            new GPUExtent3D(
+                {
+                    width: setInstruction.size.width,
+                    height: setInstruction.size.height,
+                    depthOrArrayLayers: setInstruction.size.depthOrArrayLayers,
+                }
+            )
+        );
+    }
+    // #setUniformBinding(descriptor) {}
+    #setResourceBinding(resourceBinding, setInstruction) {
+        const resourceDescriptor = resourceBinding.resourceDescriptor;
+        const bindGroupLayoutEntry = resourceBinding.bindGroupLayoutEntry;
+        const bindGroupEntry = resourceBinding.bindGroupEntry;
+
+        switch (resourceDescriptor.type) {
+            case BufferDescriptor.DEFAULT.TYPE:
+                this.#setBufferBinding(resourceDescriptor, setInstruction);
+                break;
+            case TextureDescriptor.DEFAULT.TYPE:
+                this.#setTextureBinding(resourceDescriptor, setInstruction);
+                break;
+            case SamplerDescriptor.DEFAULT.TYPE:
+                // this.#setSamplerBinding();
+                break;
+            default:
+                throw new Error(`Unknown resource type: [${resourceDescriptor.target}]!`);
+        }
+    }
+};
