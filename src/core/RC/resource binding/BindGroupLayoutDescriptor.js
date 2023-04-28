545d2ae01adc6605f807d297c3a67c95e094b791
ss4774 <ss4774@student.uni-lj.si>
2023-04-28 12:17:35 +0200

    Added static configuration

--- src/core/RC/resource binding/BindGroupLayoutDescriptor.js
+++ src/core/RC/resource binding/BindGroupLayoutDescriptor.js
@@ -1,4 +1,5 @@
 import { DescriptorBase } from "../DescriptorBase.js";
+import { BindGroupLayoutEntry } from "./BindGroupLayoutEntry.js";
 
 
 export class BindGroupLayoutDescriptor extends DescriptorBase {
@@ -6,6 +7,86 @@ export class BindGroupLayoutDescriptor extends DescriptorBase {
 
     static DEFAULT = {
         ENTRIES: undefined,
+    };
+
+    static CONFIGURATION = {
+        BGLD00: {
+            BGLEs_Bx02_Tx00_Sx00: new BindGroupLayoutDescriptor(
+                {
+                    label: "[BGLD00.BGLEs_Bx02_Tx00_Sx00]",
+                    entries: [
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE00_VVF_RB_TU,
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE01_VVF_RB_TU,
+                    ]
+                }
+            )
+        },
+        BGLD01: {
+            BGLEs_Bx01_Tx00_Sx00: new BindGroupLayoutDescriptor(
+                {
+                    label: "[BGLD01.BGLEs_Bx01_Tx00_Sx00]",
+                    entries: [
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE10_VVF_RB_TU,
+                    ]
+                }
+            )
+        },
+        BGLD02: {
+            BGLEs_Bx01_Tx00_Sx00: new BindGroupLayoutDescriptor(
+                {
+                    label: "[BGLD02.BGLEs_Bx01_Tx00_Sx00]",
+                    entries: [
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE00_VVF_RB_TU,
+                    ]
+                }
+            )
+        },
+        BGLD03: {
+            BGLEs_Bx01_Tx01_Sx01: new BindGroupLayoutDescriptor(
+                {
+                    label: "[BGLD03.BGLEs_Bx01_Tx01_Sx01]",
+                    entries: [
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE00_VVF_RB_TU,
+    
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE10_VVF_RT_TF,
+                        
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE20_VVF_RS_TF,
+                    ]
+                }
+            ),
+            BGLEs_Bx01_Tx02_Sx02: new BindGroupLayoutDescriptor(
+                {
+                    label: "[BGLD03.BGLEs_Bx01_Tx02_Sx02]",
+                    entries: [
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE00_VVF_RB_TU,
+    
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE10_VVF_RT_TF,
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE11_VVF_RT_TF,
+                        
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE20_VVF_RS_TF,
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE21_VVF_RS_TF,
+                    ]
+                }
+            ),
+            BGLEs_Bx01_Tx04_Sx04: new BindGroupLayoutDescriptor(
+                {
+                    label: "[BGLD03.BGLEs_Bx01_Tx04_Sx04]",
+                    entries: [
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE00_VVF_RB_TU,
+    
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE10_VVF_RT_TF,
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE11_VVF_RT_TF,
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE12_VVF_RT_TF,
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE13_VVF_RT_TF,
+                        
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE20_VVF_RS_TF,
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE21_VVF_RS_TF,
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE22_VVF_RS_TF,
+                        BindGroupLayoutEntry.CONFIGURATION.BGLE23_VVF_RS_TF,
+                    ]
+                }
+            )
+        },
     };
 
 
