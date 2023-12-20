89c7e2124c4d1ec5d7d4ea2ba01592ecee0ddc0b
ss4774 <ss4774@student.uni-lj.si>
2023-12-20 22:42:15 +0100

    Added map t2 definition

---
+++ src/core/MapT2.js
@@ -0,0 +1,39 @@
+import { MapObjectBase } from "./MapObjectBase.js";
+
+
+export class MapT2 extends MapObjectBase {
+
+
+	static DEFAULT = {
+		TYPE: "MapT2",
+		NAME: "",
+	};
+
+
+	constructor(args = {}) {
+		super(
+			{
+				...args,
+
+				type: (args.type !== undefined) ? args.type : MapT2.DEFAULT.TYPE,
+				name: (args.name !== undefined) ? args.name : MapT2.DEFAULT.NAME,
+			}
+		);
+	}
+
+
+	clone() {
+		const originalMap = this;
+		const clonedMap = new MapT2();
+
+		originalMap.forEach((value, key) => {
+			const clonedKey = (key === Object(key)) ? x.clone() : key;
+			const clonedValue = (value === Object(value)) ? value.clone() : value;
+
+			clonedMap.set(clonedKey, clonedValue);
+		});
+	
+
+		return clonedMap;
+	}
+};
